29a2556b3e207c716f15622a6c0b51aa34e67fe2
diptesh17 <deorediptesh1883@gmail.com>
2024-01-27 00:10:58 +0530

    Added jwt create and verify logic

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,4 +5,5 @@ dotenv.config();
 module.exports = {
   PORT: process.env.PORT,
   SALT: bcrypt.genSaltSync(10),
+  JWT_KEY: process.env.JWT_KEY,
 };
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,6 @@
 const UserRepository = require("../repository/user-repository");
-
+const jwt = require("jsonwebtoken");
+const { JWT_KEY } = require("../config/serverConfig");
 class UserService {
   constructor() {
     this.UserRepository = new UserRepository();
@@ -11,6 +12,26 @@ class UserService {
       return user;
     } catch (error) {
       console.log("Something went wrong in service layer");
+      throw error;
+    }
+  }
+
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1h" });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in Token creation");
+      throw error;
+    }
+  }
+
+  verifyToken(user) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in Token Validation");
       throw error;
     }
   }
